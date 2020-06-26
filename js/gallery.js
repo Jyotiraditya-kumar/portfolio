1f41f52bab30adc7593fbdb365153db099f9e87a
Jyotiraditya Kumar <64402728+Jyotiraditya-kumar@users.noreply.github.com>
2020-06-26 16:37:00 +0530

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,30 +1,32 @@
+function upDate(previewPic) {
+    x = document.getElementById("image");
+    z = document.getElementById("lview");
+    x.src = previewPic.src;
+    z.style.display = "block";
 
-
-function upDate(previewPic){
-    x=document.getElementById("image");
-    z=document.getElementById("lview");
-    x.src=previewPic.src;
-    z.style.display="block";
-      
 }
-   
-function unDo(){
-       x=document.getElementById("image");
-       z=document.getElementById("lview");
-       z.style.display="none";
-       x.src="";
-      
-           
-       }
-function menu()
-{
-    x=document.getElementById("collapse");
-    if (x.style.display=="none")
-    {
-        x.style.display="flex";
+
+function unDo() {
+    x = document.getElementById("image");
+    z = document.getElementById("lview");
+    z.style.display = "none";
+    x.src = "";
+
+
+}
+
+function openNav() {
+    x = document.getElementById("collapse");
+    if (window.matchMedia("(max-width:400px)").matches) {
+        x.style.width = "100px";
+    } else if (window.matchMedia("(max-width:600px)").matches) {
+        x.style.width = "150px"
     }
-    else
-    {
-        x.style.display="none";
-    }
+
+
+}
+
+function closeNav() {
+    x = document.getElementById("collapse");
+    x.style.width = "0";
 }
---
+++ js/google-map.js
@@ -0,0 +1,15 @@
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(28.713525, 77.751650),
+        zoom: 5,
+    };
+    myCenter = { lat: 28.713525, lng: 77.751650 };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var marker = new google.maps.Marker({
+        position: myCenter,
+        title: "Jyotiraditya",
+
+    });
+
+    marker.setMap(map);
+}
